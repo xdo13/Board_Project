3df256695742464a58fe3fef2dcadd96ce42c286
xdo13 <xdo13@naver.com>
2024-11-29 16:40:49 +0900

    게시판 상세보기 기능 추가

--- src/main/java/model2/mvcboard/ViewController.java
+++ src/main/java/model2/mvcboard/ViewController.java
@@ -6,14 +6,39 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet("/mvcboard/view.do")
 public class ViewController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//게시물 불러오기
 		MVCBoardDAO dao = new MVCBoardDAO();
 		String idx = req.getParameter("idx");
+		dao.updateVisitCount(idx);
+		MVCBoardDTO dto = dao.selectView(idx);
+		dao.close();
 		
+		//줄바꿈 처리
+		dto.setContent(dto.getContent().replaceAll("\r\n", "<br/>"));
+		
+		//첨부 파일 확장자 추출 및 이미지 타입 확인
+		String ext = null, fileName = dto.getSfile();
+		if(fileName!=null) {
+			ext = fileName.substring(fileName.lastIndexOf(".")+1);
+		}
+		String[] mimeStr = {"png","jpg","gif"};
+		List<String> mimeList = Arrays.asList(mimeStr);
+		boolean isImage = false;
+		if(mimeList.contains(ext)) {
+			isImage = true;
+		}
+		
+		//게시물(dto) 저장 후 뷰로 포워드
+		req.setAttribute("dto", dto);
+		req.setAttribute("isImage", isImage);
+		req.getRequestDispatcher("/mvcboard/View.jsp").forward(req, res);
 		
 
 	}
